971946f61b29be19210c1ae1244707b5453b6c60
Ameli98 <rain0210303@gmail.com>
2024-05-31 15:07:51 +0800

    feat : Snap point for drawing pair

--- DrawLinePair.py
+++ DrawLinePair.py
@@ -1,46 +1,95 @@
 import cv2
 import numpy as np
-from PIL import Image
+import random
 import csv
 import argparse
 from time import time
 
-SrcPointBuffer, DstPointBuffer = [], []
-t0, t1 = time(), time()
+
+class Point():
+    def __init__(self, x:int, y:int) -> None:
+        self.Coordinate = (x, y)
+
+    def __getitem__(self, key:int) -> int:
+        return self.Coordinate[key]
+
+class Line():
+    def __init__(self, Point1:Point, Point2:Point) -> None:
+        self.Points = (Point1, Point2)
+
+    def __getitem__(self, key:int) -> Point:
+        return self.Points[key]
+
+
+class LinePairList():
+    def __init__(self) -> None:
+        self.SrcLineList = []
+        self.DstLineList = []
+        self.SrcPointBuffer, self.DstPointBuffer = None, None
+    
+    def __len__(self) -> int:
+        return min(len(self.SrcLineList), len(self.DstLineList))
+    
+    def __getitem__(self, key) -> tuple:
+        return self.SrcLineList[key], self.DstLineList[key]
+
+    def SrcAppend(self, p:Point):
+        if self.SrcPointBuffer:
+            self.SrcLineList.append(Line(self.SrcPointBuffer, p))
+            p1 = self.SrcPointBuffer
+            self.SrcPointBuffer = None
+            return p1, p
+        else:
+            self.SrcPointBuffer = p
+            return None
+        
+    def DstAppend(self, p:Point):
+        if self.DstPointBuffer:
+            self.DstLineList.append(Line(self.DstPointBuffer, p))
+            p1 = self.DstPointBuffer
+            self.DstPointBuffer = None
+            return p1, p
+        else:
+            self.DstPointBuffer = p
+            return None
+        
+    def GetSrcLen(self) -> int:
+        return len(self.SrcLineList)
+    
+    def GetDstLen(self) -> int:
+        return len(self.DstLineList)
+
 
 def ColorGenerator(seed):
-    np.random.seed(seed)
-    return list(np.random.choice(range(256), size=3))
+    random.seed(seed)
+    return random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)
 
-def DrawSrcImage(event, x, y, flags, param):
-    global SrcPointBuffer, t0, t1
-    if event == cv2.EVENT_LBUTTONDOWN:
+def DrawImage(event, x, y, flags, param):
+    global LinePairs, t0, t1
+    img, src = param[0], param[1]
+
+    if event == cv2.EVENT_LBUTTONDOWN or event == cv2.EVENT_LBUTTONDBLCLK:
         Now = time()
         if Now - t1 > 0.5:
             t0, t1 = t1, Now
         else:
             return
-        SrcPointBuffer.extend([y, x])
-        print('clicking: ', x, y)
-        cv2.circle(param, (x, y), 5, (255, 255, 255), thickness=-1)  # Slightly bigger circle for visibility
-        if len(SrcPointBuffer) % 4 == 0:
-            Color = ColorGenerator(len(SrcPointBuffer) // 4)
-            cv2.line(param, (SrcPointBuffer[-3], SrcPointBuffer[-4]), (SrcPointBuffer[-1], SrcPointBuffer[-2]), (int(Color[0]), int(Color[1]), int(Color[2])), 2)
+        Pixel = Point(x, y)
+        cv2.circle(img, (x, y), 5, (255, 255, 255), thickness=-1)
 
-def DrawDstImage(event, x, y, flags, param):
-    global DstPointBuffer, t0, t1
-    if event == cv2.EVENT_LBUTTONDOWN:
-        Now = time()
-        if Now - t1 > 0.5:
-            t0, t1 = t1, Now
-        else:
-            return
-        DstPointBuffer.extend([y, x])
-        print('clicking: ', x, y)
-        cv2.circle(param, (x, y), 5, (255, 255, 255), thickness=-1)  # Slightly bigger circle for visibility
-        if len(DstPointBuffer) % 4 == 0:
-            Color = ColorGenerator(len(DstPointBuffer) // 4)
-            cv2.line(param, (DstPointBuffer[-3], DstPointBuffer[-4]), (DstPointBuffer[-1], DstPointBuffer[-2]), (int(Color[0]), int(Color[1]), int(Color[2])), 2)
+        def AddOnce():
+            Link = LinePairs.SrcAppend(Pixel) if src else LinePairs.DstAppend(Pixel)
+            if Link:
+                i = LinePairs.GetSrcLen() if src else LinePairs.GetDstLen()
+                Color = ColorGenerator(i)
+                cv2.line(img, Link[0].Coordinate, Link[1].Coordinate, (int(Color[0]), int(Color[1]), int(Color[2])), 2)
+        AddOnce()
+        if flags == cv2.EVENT_FLAG_SHIFTKEY + cv2.EVENT_LBUTTONDOWN:
+            AddOnce()
+
+
+LinePairs = LinePairList()
+t0, t1 = time(), time()
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -50,19 +99,18 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     SrcImage = cv2.imread(args.src)
-    DstImage = Image.open(args.dst)
-    DstImage = DstImage.resize((SrcImage.shape[1], SrcImage.shape[0]))
-    DstImage = cv2.cvtColor(np.array(DstImage), cv2.COLOR_RGB2BGR)
+    DstImage = cv2.imread(args.dst)
+    DstImage = cv2.resize(DstImage, (SrcImage.shape[1], SrcImage.shape[0]), interpolation=cv2.INTER_CUBIC)
 
     if SrcImage is None or DstImage is None:
         print("Error: One of the images could not be loaded.")
         exit()
 
     cv2.namedWindow('SrcImage')
-    cv2.setMouseCallback('SrcImage', DrawSrcImage, SrcImage)
+    cv2.setMouseCallback('SrcImage', DrawImage, (SrcImage, True))
 
     cv2.namedWindow('DstImage')
-    cv2.setMouseCallback('DstImage', DrawDstImage, DstImage)
+    cv2.setMouseCallback('DstImage', DrawImage, (DstImage, False))
     
     while(True):
         cv2.imshow('SrcImage', SrcImage)
@@ -73,5 +121,9 @@ if __name__ == "__main__":
 
     with open(args.csv, "w", newline="") as csvfile:
         writer = csv.writer(csvfile)
-        writer.writerow(["Src1y", "Src1x", "Src2y", "Src2x", "Dst1y", "Dst1x", "Dst2y", "Dst2x"])
-        writer.writerows(list(zip(SrcPointBuffer[::4],SrcPointBuffer[1::4], SrcPointBuffer[2::4],SrcPointBuffer[3::4], DstPointBuffer[::4], DstPointBuffer[1::4], DstPointBuffer[2::4], DstPointBuffer[3::4])))
+        writer.writerow(["Srcy1", "Srcx1", "Srcy2", "Srcx2", "Dsty1", "Dstx1", "Dsty2", "Dstx2"])
+        for SrcLine, DstLine in LinePairs:
+            SrcL1, SrcL2, DstL1, DstL2 = *SrcLine, *DstLine
+            SrcX1, SrcY1, SrcX2, SrcY2 = *SrcL1, *SrcL2
+            DstX1, DstY1, DstX2, DstY2 = *DstL1, *DstL2
+            writer.writerow((SrcY1, SrcX1, SrcY2, SrcX2, DstY1, DstX1, DstY2, DstX2))
