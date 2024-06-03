50d90fd2bea3da2c731dc69f8df184ee8f875d7d
Ameli98 <rain0210303@gmail.com>
2024-06-03 16:05:50 +0800

    feat : color hamonization

--- Hamonization.py
+++ Hamonization.py
@@ -9,10 +9,40 @@ def AddLabel(event, x, y, flags, Label):
     if event == cv2.EVENT_LBUTTONDOWN:
         SelectedLabels.add(int(Label[y, x]))
 
+
+def Harmonization(Image, Mask):
+    HSVImage = cv2.cvtColor(Image, cv2.COLOR_BGR2HSV)
+    H = HSVImage[..., 0].astype(np.float16)
+
+    # Find Minimum Sector in the HSV space
+    SortedH = np.sort(H[Mask], axis=None)
+    Diff = np.zeros_like(SortedH)
+    Diff[:-1] = SortedH[1:] - SortedH[:-1]
+    Diff[-1] = 256 - (SortedH[-1] - SortedH[0])
+    End = np.argmax(Diff)
+    Start = 0 if End == len(Diff) else End + 1
+
+    # Mapping Hue
+    SectorArc = End - Start if End > Start else 256 + (End - Start)
+    if SectorArc < 0:
+        SectorArc += 256
+        H[H <= End] += 256
+
+    Scale = 32 / SectorArc
+    if Scale >= 1:
+        return Image
+    else:
+        Affine = H.mean() * (1 - Scale)
+
+    H[Mask] = Scale * H[Mask] + Affine
+    
+    HSVImage[..., 0] = H.astype(np.uint8)
+    return cv2.cvtColor(HSVImage, cv2.COLOR_HSV2BGR)
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("InputPath")
-    # parser.add_argument("OutputPath")
+    parser.add_argument("OutputPath")
     args = parser.parse_args()
 
     InputImage = cv2.imread(args.InputPath)
@@ -31,3 +61,9 @@ if __name__ == "__main__":
     cv2.destroyAllWindows()
 
     # TODO: With selected labels, fetch these pixels for color hamonization
+    Mask = np.full((InputImage.shape[0], InputImage.shape[1]), False)
+    for l in SelectedLabels:
+        Mask[Label == l] = True
+
+    OutputImage = Harmonization(InputImage, Mask)
+    cv2.imwrite(args.OutputPath, OutputImage)
